25f144deaef480c1cb608387acffc935925ea70a
David Durbin <David-Durbin@users.noreply.github.com>
2019-01-25 10:09:29 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("My name is David.");
+	    System.out.println("I haven't done this in a while.");
+	    System.out.println("I had forgotten what a pain in the butt System.out.println() is to type.");
+	    System.out.println("I miss cout >> \"Hello World!\" !");
+    }
+}
